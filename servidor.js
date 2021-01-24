c891b475052e13ed96e39dc5e850117642eee8d7
Rodrigoleiroz <76019058+Rodrigoleiroz@users.noreply.github.com>
2021-01-24 14:39:22 -0300

    criando endpoint nomes aleatorios

--- servidor.js
+++ servidor.js
@@ -1,9 +1,17 @@
 const http = require("http");
 const express = require("express");
 const app = express();
+let nomesaleatorios = ['rodrigo', 'joao', 'pedro', 'lucas', 'marcos', 'antonio', 'cleber', 'eliel', 'selma', 'luisa', 'gabriel', 'bruno', 'matheus', 'madara']
 
 app.get("/", function(req, res) {
     res.send({ working: true });
+});
+
+app.get("/nomesaleatorios", function(req, res) {
+    tamanho = nomesaleatorios.length
+    numal = Math.floor(Math.random() * tamanho)
+    nome = nomesaleatorios[numal]
+    res.send({ "nome": nome, numal, tamanho });
 });
 
 http.createServer(app).listen(3000, () => console.log("Servidor rodando local na porta 3000"));
